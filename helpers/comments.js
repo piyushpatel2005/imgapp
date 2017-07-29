fe229c746a7fa8d251f8c61e1f891670070be27b
Piyush Patel <piyushpatel2005@gmail.com>
2017-07-29 14:30:23 -0400

    Add latest comments to image

--- helpers/comments.js
+++ helpers/comments.js
@@ -1,42 +1,21 @@
-module.exports = {
-  newest: function () {
-    var comments = [
-      {
-        image_id: 1,
-        email: 'test@test.com',
-        name: "Test user",
-        gravatar: "http://lorempixel.com/75/75/animals/1",
-        comment: "This is a comment",
-        timestamp: Date.now(),
-        image: {
-          uniqueId: 1,
-          title: "Sample Image 1",
-          description: "",
-          filename: "sample1.jpg",
-          views: 0,
-          likes: 0,
-          timestamp: Date.now()
-        }
-      },
-      {
-        image_id: 1,
-        email: 'test@test.com',
-        name: "Test user",
-        gravatar: "http://lorempixel.com/75/75/animals/2",
-        comment: "This is a comment",
-        timestamp: Date.now(),
-        image: {
-          uniqueId: 1,
-          title: "Sample Image 1",
-          description: "",
-          filename: "sample1.jpg",
-          views: 0,
-          likes: 0,
-          timestamp: Date.now()
-        }
-      }
-    ];
+var models = require('../models'),
+    async = require('async');
 
-    return comments;
+module.exports = {
+  newest: function (callback) {
+    models.Comment.find({}, {}, {limit: 5, sort: {timstamp: -1}}, function(err, comments) {
+      var attachImage = function(comment, next) {
+        models.Image.findOne({_id: comment.image_id }, function(err, image) {
+          if(err) throw err;
+          comment.image = image;
+          next(err);
+        });
+      };
+
+      async.each(comments, attachImage, function(err) {
+        if(err) throw err;
+        callback(err, comments);
+      });
+    });
   }
-}
+};
--- helpers/sidebar.js
+++ helpers/sidebar.js
@@ -2,12 +2,27 @@ var Stats = require("./stats"),
     Images = require("./images"),
     Comments = require("./comments");
 
-module.exports = function(viewModel, callback) {
-  viewModel.sidebar = {
-    stats: Stats(),
-    popular: Images.popular(),
-    comments: Comments.newest()
-  };
+var async = require('async');
 
-  callback(viewModel);
+module.exports = function(viewModel, callback) {
+
+  async.parallel([
+    function(next) {
+      next(null, Stats());
+    },
+    function(next) {
+      next(null, Images.popular());
+    },
+    function(next) {
+      Comments.newest(next);
+    }
+  ], function(err, results) {
+    viewModel.sidebar = {
+      stats: results[0],
+      popular: results[1],
+      comments: results[2]
+    };
+
+    callback(viewModel);
+  });
 };
